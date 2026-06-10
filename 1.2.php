37bbd0367529229d2be3a5bfd36d8f3631427ee9
rachitchauhan134006-create <rachit.chauhan134006@marwadiuniversity.ac.in>
2026-06-10 12:44:14 +0530

    Add files via upload

---
+++ 1.2.php
@@ -0,0 +1,22 @@
+<?php
+
+$num1 = 45;
+$num2 = 78;
+$num3 = 32;
+
+$max = max($num1, $num2, $num3);
+$min = min($num1, $num2, $num3);
+
+echo "----------------------------------<br>";
+echo "!! MAXIMUM AND MINIMUM NUMBER !!<br>";
+echo "----------------------------------<br>";
+echo "Number 1 : " . $num1 . "<br>";
+echo "Number 2 : " . $num2 . "<br>";
+echo "Number 3 : " . $num3 . "<br>";
+echo "----------------------------------<br>";
+echo "Maximum Number : " . $max . "<br>";
+echo "Minimum Number : " . $min . "<br>";
+echo "----------------------------------<br>";
+echo "!! THANK YOU !!";
+
+?>
