a6b6acf530d3e35299ace47ed3ee0609d44a84b2
rachitchauhan134006-create <rachit.chauhan134006@marwadiuniversity.ac.in>
2026-06-12 12:08:45 +0530

    Add files via upload

---
+++ LAB4-1.6.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Using While Loop <br>";
+
+$i = 15;
+while($i <= 20)
+{
+    echo $i . "<br>";
+    $i++;
+}
+
+echo "<br>Using Do While Loop <br>";
+
+$j = 15;
+do
+{
+    echo $j . "<br>";
+    $j++;
+}
+while($j <= 20);
+
+?>
